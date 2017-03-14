c2f54344ef482adcd69755ff1138e7a1d14fb7a5
Vishaal <vishaal16119@iiitd.ac.in>
2017-03-15 03:38:14 +0530

    Eigth

---
+++ mail.py
@@ -0,0 +1,45 @@
+import imaplib
+import email
+import time
+from espeak import espeak
+
+user="vishaal16119@iiitd.ac.in"
+password="nagpaltasveer"
+mail=imaplib.IMAP4_SSL('imap.gmail.com')
+(retcode, capabilities)=mail.login(user,password)
+mail.list()
+mail.select("INBOX")
+mail_from_store=[]
+mail_subject_store=[]
+
+n=0
+(retcode, messages)=mail.search(None,'(UNSEEN)')
+if(retcode=='OK'):
+	for num in messages[0].split():
+		n+=1
+		result,data=mail.fetch(num,'(RFC822)')
+		for r in data:
+			if(isinstance(r,tuple)):
+				o=email.message_from_bytes(r[1])
+				print(o['From'])
+				print(o['Subject'])
+				mail_from_store.append(o['From'])
+				mail_subject_store.append(o['Subject'])
+				print()
+				result,data=mail.store(num,'+FLAGS','\\Seen')
+
+
+print(n)
+s='Vishaal, You have '+str(n)+' unread mails       Do you want me to read the names of the senders and the subjects of the mails?'
+espeak.synth(s)
+time.sleep(10)
+
+
+ch=input()
+if(ch=='no'):
+	exit()
+elif(ch=='yes'):
+	for i in range(len(mail_from_store)):
+		s1='You have a mail from '+str(mail_from_store[i])+' and the subject is '+str(mail_subject_store[i])
+		espeak.synth(s1)
+		time.sleep(10)
---
+++ mail1.py
@@ -0,0 +1,26 @@
+import imaplib
+import email
+#from espeak import espeak 
+
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
+(retcode, capabilities) = mail.login('vishaal16119@iiitd.ac.in','nagpaltasveer')
+mail.list()
+mail.select('INBOX')
+
+n=0
+(retcode, messages) = mail.search(None, '(UNSEEN)')
+if retcode == 'OK':
+
+   for num in messages[0].split() :
+      print ('Processing ')
+      n=n+1
+      typ, data = mail.fetch(num,'(RFC822)')
+      for response_part in data:
+         if isinstance(response_part, tuple):
+             original = email.message_from_string(response_part[1])
+
+             print (original['From'])
+             print (original['Subject'])
+             typ, data = mail.store(num,'+FLAGS','\\Seen')
+
+print (n)
