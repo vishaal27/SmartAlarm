0dd022699f5b0199bb5d23e4a593b932782cd756
Vishaal Udandarao <vishaal16119@iiitd.ac.in>
2017-03-21 14:37:46 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -11,7 +11,7 @@ def time_converter(time):
 
 
 def url_builder(city_id):
-    user_api = '3ce8aff62271ce0fbe28063131cb2376'  # Obtain yours form: http://openweathermap.org/
+    user_api = '3ce8affy62y27x1ce0fbey2x8063131cxb237xx6xxxxxx'  # Obtain yours form: http://openweathermap.org/
     unit = 'metric'  # For Fahrenheit use imperial, for Celsius use metric, and the default is Kelvin.
     api = 'http://api.openweathermap.org/data/2.5/weather?id='     # Search for your city ID here: http://bulk.openweathermap.org/sample/city.list.json.gz
 
