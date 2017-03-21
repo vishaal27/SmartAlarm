e637f50ca6ed403bbce044d38edc68ba1b13f3bd
Vishaal Udandarao <vishaal16119@iiitd.ac.in>
2017-03-21 14:37:13 +0530

    Update seleniumscript.py

--- seleniumscript.py
+++ seleniumscript.py
@@ -7,7 +7,7 @@ driver.find_element_by_id("loginButton").click()
 username=driver.find_element_by_id("user_email")
 username.send_keys("vishaal16119@iiitd.ac.in")
 password=driver.find_element_by_id("user_password")
-password.send_keys("nagpaltasveer")
+password.send_keys("")
 username.submit()
 
 
