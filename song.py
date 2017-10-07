9f9d282573e8344c6e165d9de3e0456287f72cb0
Vishaal Udandarao <vishaal16119@iiitd.ac.in>
2017-10-07 16:14:27 +0530

    Update song.py

--- song.py
+++ song.py
@@ -2,7 +2,7 @@
 import pygame
 pygame.mixer.pre_init(44100, -16, 2, 2048) # setup mixer to avoid sound lag
 pygame.init()
-file='/home/vishaal/Desktop/Music/faded.mp3'
+file='' #TODO: retrieve any online file.
 pygame.mixer.init()
 pygame.mixer.music.load(file)
 pygame.mixer.music.play(-1)
