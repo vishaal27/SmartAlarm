a8c922919b65690966c58195a658c9d0e9669c12
Amrut <amrut.phadke2016@vitstudent.ac.in>
2017-10-10 19:15:35 +0530

    Added pyjokes module.
    added pyjokes module to generate Top quality programmer jokes.
    You can refer to their Official GitHub repository here : https://github.com/pyjokes/pyjokes/
    Read the Docs Here : http://pyjok.es/install/ 
    to install : pip install pyjokes

--- joke.py
+++ joke.py
@@ -2,12 +2,21 @@ import random
 import time
 from espeak import espeak
 import requests
+import pyjokes
 import json
 
-#parses the response received from the api to json format
+# parses the response received from the api to json format
 response = requests.get("https://icanhazdadjoke.com/", headers={"Accept":"application/json"}).json()
 
-#assigns the gets the joke from the parsed json and assigns to chosen
+# no need to use unneccessary if statements. just a call to pyjoke.
+# first parameter for language, second for category. refer docs for more info.
+random_adult_joke = pyjokes.get_joke('en','adult')
+random_neutral_joke = pyjokes.get_joke()
+
+# get list of jokes for a particular Genre.
+chuck_norris_jokes = pyjokes.get_jokes('en','chuck')
+                                     
+# assigns the gets the joke from the parsed json and assigns to chosen
 chosen=response["joke"]
 if(chosen[1]=='0' or chosen[1]=='1' or chosen[1]=='2' or chosen[1]=='3' or chosen[1]=='4' or chosen[1]=='5' or chosen[1]=='6' or chosen[1]=='7' or chosen[1]=='8' or chosen[1]=='9'):
     speak=chosen[4:]
@@ -18,4 +27,3 @@ espeak.synth(speak)
 time.sleep(7)
 espeak.synth('Haha')
 time.sleep(3)
-
